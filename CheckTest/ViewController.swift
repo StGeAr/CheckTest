0498322f55653b3fd483bd77a0b29cd3f6489b8c
German Stavitskiy <gera_stavickii@mail,.ru>
2022-03-25 22:47:14 +0300

    Initial Commit

---
+++ CheckTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckTest
+//
+//  Created by Герман Ставицкий on 25.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
