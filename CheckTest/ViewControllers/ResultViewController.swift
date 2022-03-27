8853d958d293c122267db6f3dd4e9e6ffd233ddc
German Stavitskiy <gera_stavickii@mail,.ru>
2022-03-27 12:56:27 +0300

    setup resultVC

---
+++ CheckTest/ViewControllers/ResultViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ResultViewController.swift
+//  CheckTest
+//
+//  Created by Герман Ставицкий on 27.03.2022.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    @IBOutlet var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func showCorrectAnswersButton() {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
