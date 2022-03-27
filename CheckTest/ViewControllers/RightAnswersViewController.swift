25ea977cbad3031b63a61f0dc7aea5f54acb1bcd
Kirill <frolkv@gmail.com>
2022-03-27 12:50:41 +0300

    Kirill's new branch

---
+++ CheckTest/ViewControllers/RightAnswersViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RightAnswersViewController.swift
+//  CheckTest
+//
+//  Created by Кирилл on 27.03.2022.
+//
+
+import UIKit
+
+class RightAnswersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+//Всем привет!!!
+
+}
