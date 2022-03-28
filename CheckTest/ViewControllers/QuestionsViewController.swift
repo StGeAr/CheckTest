1328e5d0a9e89d89076073b6561b93003b461a83
German Stavitskiy <gera_stavickii@mail,.ru>
2022-03-29 01:28:04 +0300

    correct questionVC and resultVC

--- CheckTest/ViewControllers/QuestionsViewController.swift
+++ CheckTest/ViewControllers/QuestionsViewController.swift
@@ -11,66 +11,55 @@ class QuestionsViewController: UIViewController {
 
     @IBOutlet var buttons: [UIButton]!
     @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var questionProgressLine: UIProgressView!
     
     private let questions = Question.getQuestion()
     private var questionIndex = 0
-//    private var answersChosen: [Answer] = []
-    private var answersChosen = [
-        "БУСТ",
-        "К-18",
-        "К-9",
-        "К-22",
-        "Независимая",
-        "жопа",
-        "Вилка",
-        "1/15 и 1/6",
-        "Менее 650В",
-        "Маневровый"
-    ]
-    
-    private var currentAnswer: [Answer] {
+    private var answersChosen: [Answer] = []
+    private var currentAnswers: [Answer] {
         questions[questionIndex].answers
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        updateUI()
+        updateUI()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let resultVC = segue.destination as? ResultsViewController else { return }
-        resultVC.currentAnswers = answersChosen
+        resultVC.answersChosen = answersChosen
     }
     
-//    @IBAction func answerButtonPressed(_ sender: UIButton) {
-//        guard let currentIndex = buttons.firstIndex(of: sender) else { return }
-//        let currentAnswer = currentAnswers[currentIndex]
-//        answerChosen.append(currentAnswer)
-//        nextQuestion()
-//
-//    }
-//}
-//
-//extension QuestionsViewController {
-//    private func updateUI() {
-//        let currentQuestion  = questions[questionIndex]
-//        questionLabel.text = currentQuestion.title
-//        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
-//        showCurrentAnswers()
-//    }
-//
-//    private func showCurrentAnswers(with answers: currentAnswer) {
-//        for (button, answer) in zip(buttons, answers) {
-//            button.setTitle(answer.title, for: .normal)
-//        }
-//    }
-//
-//    private func nextQuestion() {
-//        questionIndex += 1
-//        if questionIndex < questions.count {
-//            updateUI()
-//            return
-//        }
-//        performSegue(withIdentifier: "showResult", sender: nil)
-//    }
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        guard let currentIndex = buttons.firstIndex(of: sender) else { return }
+        let currentAnswer = currentAnswers[currentIndex]
+        answersChosen.append(currentAnswer)
+        nextQuestion()
+    }
+}
+
+extension QuestionsViewController {
+    private func updateUI() {
+        let currentQuestion = questions[questionIndex]
+        questionLabel.text = currentQuestion.title
+        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+        let totalProgress = Float(questionIndex) / Float(questions.count)
+        questionProgressLine.setProgress(totalProgress, animated: true)
+        showCurrentAnswers(with: currentAnswers)
+    }
+
+    private func showCurrentAnswers(with answers: [Answer]) {
+        for (button, answer) in zip(buttons, answers) {
+            button.setTitle(answer.title, for: .normal)
+        }
+    }
+
+    private func nextQuestion() {
+        questionIndex += 1
+        if questionIndex < questions.count {
+            updateUI()
+            return
+        }
+        performSegue(withIdentifier: "showResult", sender: nil)
+    }
 }
--- CheckTest/ViewControllers/ResultsViewController.swift
+++ CheckTest/ViewControllers/ResultsViewController.swift
@@ -17,7 +17,7 @@ class ResultsViewController: UIViewController {
         }
     }
 
-    var currentAnswers: [String]!
+    var answersChosen: [Answer]!
     var correctAnswers: [String] {
         let questions = Question.getQuestion()
         var answers: [String] = []
@@ -26,27 +26,28 @@ class ResultsViewController: UIViewController {
         }
         return answers
     }
-
+    
     // MARK: - Этот геттер я отправлять буду Кириллу
-    private var incorrectAnsweredQuestions: [Int] {
-        var questionNumber: [Int] = []
-        for currentAnswer in currentAnswers {
-            if !correctAnswers.contains(currentAnswer) {
-                if let index = currentAnswers.firstIndex(of: currentAnswer) {
-                    questionNumber.append(index)
-                }
-            }
-        }
-        return questionNumber
-    }
+//    private var incorrectAnsweredQuestions: [Int] {
+//        let currentAnswers = answersChosen.map { $0.title }
+//        var questionNumber: [Int] = []
+//        for currentAnswer in currentAnswers {
+//            if !correctAnswers.contains(currentAnswer) {
+//                if let index = currentAnswers.firstIndex(of: currentAnswer) {
+//                    questionNumber.append(index)
+//                }
+//            }
+//        }
+//        return questionNumber
+//    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.hidesBackButton = true
         updateResultInfo()
     }
-    
-//    // MARK: - Navigation
+
+    // MARK: - Navigation
 //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 //        guard
 //            let navigationVC = segue.destination as? UINavigationController
@@ -60,24 +61,24 @@ class ResultsViewController: UIViewController {
 //        }
 //            correctAnswersVC.answers = incorrectAnsweredQuestions
 //    }
-        
+
     private func updateResultInfo() {
+        let currentAnswers = answersChosen.compactMap { $0.title }
         if currentAnswers == correctAnswers {
             resultTypeLabel.text = "Поздравляем! Вы ответили на все вопросы!"
             resultNumberLabel.text = "Верно \(currentAnswers.count) из \(correctAnswers.count)"
         } else {
             var answers: [String] = []
-            
+
             for currentAnswer in currentAnswers {
                 if correctAnswers.contains(currentAnswer) {
                     answers.append(currentAnswer)
                 }
             }
-            
+
             resultTypeLabel.text = "Упс! Вам необходимо повторить теорию!"
             resultNumberLabel.text = "Верно \(answers.count) из \(correctAnswers.count)"
             correctAnswersButton.isHidden = false
         }
     }
-
 }
