163c8209d225f9022672c9f56a855c0ccfd99af5
Dmitriy Deyashkin <d-deyashkin@yandex.ru>
2022-03-31 20:22:55 +0300

    Dima_change_names

---
+++ CheckTest/Theme/Theme.swift
@@ -0,0 +1,18 @@
+//
+//  Theme.swift
+//  CheckTest
+//
+//  Created by D D on 30.03.2022.
+//
+
+import UIKit
+
+protocol ThemeProtocol {
+    
+    var backgroundColor: UIColor { get }
+    var textColor: UIColor { get }
+}
+
+class Theme {
+    static var currentTheme: ThemeProtocol = LightTheme()
+}
