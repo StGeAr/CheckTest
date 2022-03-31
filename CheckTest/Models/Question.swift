d6b1bbc381d5043862801eb5c5395a5afa2a5a5a
German Stavitskiy <gera_stavickii@mail,.ru>
2022-03-31 20:39:03 +0300

    some commit

---
+++ CheckTest/Models/Question.swift
@@ -0,0 +1,12 @@
+//
+//  Question.swift
+//  CheckTest
+//
+//  Created by Masaie on 27/3/22.
+//
+
+import Foundation
+
+struct Question {
+    
+}
---
+++ CheckTest/Services/DataManager.swift
@@ -0,0 +1,12 @@
+//
+//  DataManager.swift
+//  CheckTest
+//
+//  Created by Masaie on 27/3/22.
+//
+
+import Foundation
+
+class DataManager {
+    
+}
