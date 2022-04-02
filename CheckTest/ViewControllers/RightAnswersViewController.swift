63af2bd16690ae9bb99e53e793e19df32338ca37
German Stavitskiy <gera_stavickii@mail,.ru>
2022-04-02 14:23:09 +0300

    preFinal

--- CheckTest/ViewControllers/RightAnswersViewController.swift
+++ CheckTest/ViewControllers/RightAnswersViewController.swift
@@ -15,54 +15,53 @@ class RightAnswersViewController: UITableViewController {
     }
     
     //MARK: - Public properties
-        var incorrectAnsweredQuestionNumbers: [Int]!
-        let questions = Question.getQuestion()
-        
-        //MARK: - Life cycles methods
-        override func numberOfSections(in tableView: UITableView) -> Int {
-            incorrectAnsweredQuestionNumbers.count
-        }
-        
-        override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-            let questionNumber = incorrectAnsweredQuestionNumbers[section] + 1
-            return "Вопрос №\(questionNumber)"
-            
-        }
+    var incorrectAnsweredQuestionNumbers: [Int]!
+    let questions = Question.getQuestion()
     
-        override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
-            if let headerView = view as? UITableViewHeaderFooterView {
-                headerView.textLabel?.textColor = .red
-                }
+    //MARK: - Life cycles methods
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        incorrectAnsweredQuestionNumbers.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let questionNumber = incorrectAnsweredQuestionNumbers[section] + 1
+        return "Вопрос №\(questionNumber)"
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        if let headerView = view as? UITableViewHeaderFooterView {
+            headerView.textLabel?.textColor = .red
         }
-            
-        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            2
-        }
-
-        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "showCorrectAnswers", for: indexPath)
-            let question = questions[incorrectAnsweredQuestionNumbers[indexPath.section]]
-            var content = cell.defaultContentConfiguration()
-            
-            switch indexPath.row {
-            case 0:
-                content.text = question.title
-            default:
-                content.text = "Правильный ответ: \(question.correctAnswer)"
-            }
-            
-            content.textProperties.color = Theme.currentTheme.textColor
-            cell.backgroundColor = Theme.currentTheme.backgroundColor
-            cell.selectionStyle = .none
-            
-            cell.contentConfiguration = content
-            
-            return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "showCorrectAnswers", for: indexPath)
+        let question = questions[incorrectAnsweredQuestionNumbers[indexPath.section]]
+        var content = cell.defaultContentConfiguration()
+        
+        switch indexPath.row {
+        case 0:
+            content.text = question.title
+        default:
+            content.text = "Правильный ответ: \(question.correctAnswer)"
         }
         
-        override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            tableView.deselectRow(at: indexPath, animated: true)
-        }
+        content.textProperties.color = Theme.currentTheme.textColor
+        cell.backgroundColor = Theme.currentTheme.backgroundColor
+        cell.selectionStyle = .none
+        
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
 }
 
